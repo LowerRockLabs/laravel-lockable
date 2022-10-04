2707a985fc02d455ec97d22e1c99592eb6b71c5c
lrljoe <lrljoe@users.noreply.github.com>
2022-10-04 22:30:32 +0000

    Fix styling

--- tests/LockableTest.php
+++ tests/LockableTest.php
@@ -6,12 +6,9 @@ it('can test', function () {
     expect(true)->toBeTrue();
 });
 
-
 it('Can create a user', function () {
     // Prepare
     $user = User::factory()->create(['name' => 'TestUser']);
 
-
     $this->assertSame('TestUser', $user->name);
-
 });
