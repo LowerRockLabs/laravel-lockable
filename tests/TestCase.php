6947e4c91ef4dad7c5b7db91366a8d685f0a51d8
Joe <joe@lowerrocklabs.com>
2022-10-04 23:34:42 +0100

    Test - can create a user

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,10 +4,14 @@ namespace LowerRockLabs\Lockable\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use LowerRockLabs\Lockable\LockableServiceProvider;
-use Orchestra\Testbench\TestCase as Orchestra;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+//use Orchestra\Testbench\TestCase as Orchestra;
 
-class TestCase extends Orchestra
+//class TestCase extends Orchestra
+class TestCase extends BaseTestCase
 {
+    use CreatesApplication;
+
     protected function setUp(): void
     {
         parent::setUp();
