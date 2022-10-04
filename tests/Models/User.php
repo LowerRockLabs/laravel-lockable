9f114b3afa3c2f4f624485ccf9f22cce7e44a6b3
Joe <joe@lowerrocklabs.com>
2022-10-04 23:45:48 +0100

    User

---
+++ tests/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LowerRockLabs\Lockable\Tests\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+
+    public function getAuthIdentifierName()
+    {
+        return 'email';
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,22 +4,21 @@ namespace LowerRockLabs\Lockable\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use LowerRockLabs\Lockable\Tests\Models\User;
 use LowerRockLabs\Lockable\LockableServiceProvider;
 
 //use Orchestra\Testbench\TestCase as Orchestra;
 
 //class TestCase extends Orchestra
-class TestCase extends BaseTestCase
+abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
     protected function setUp(): void
     {
         parent::setUp();
-        // Set Factories
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'LowerRockLabs\\Lockable\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
+        $this->artisan('migrate', ['--database' => 'testbench']);
+        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
     }
 
     protected function getPackageProviders($app)
@@ -32,6 +31,23 @@ class TestCase extends BaseTestCase
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+
+        // Setup the right User class (using stub)
+        $app['config']->set('auth.providers.users.model', User::class);
+        $app['config']->set('auth.providers.users', [
+            'driver' => 'eloquent',
+            'model' => User::class,
+        ]);
+        $app['config']->set('auth.guards.users', [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ]);
 
         /*
         $migration = include __DIR__.'/../database/migrations/create_laravel-lockable_table.php.stub';
