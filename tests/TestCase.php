e1e408e07a041fc6fb430eb276d2bf69f61941d1
lrljoe <lrljoe@users.noreply.github.com>
2022-10-04 22:35:11 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,9 @@
 namespace LowerRockLabs\Lockable\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use LowerRockLabs\Lockable\LockableServiceProvider;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use LowerRockLabs\Lockable\LockableServiceProvider;
+
 //use Orchestra\Testbench\TestCase as Orchestra;
 
 //class TestCase extends Orchestra
