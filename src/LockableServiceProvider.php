957e992a143959683082bae7f542efa3f19e824d
lrljoe <joe@lowerrocklabs.com>
2022-10-05 01:14:45 +0100

    Adding Model into SP

--- src/LockableServiceProvider.php
+++ src/LockableServiceProvider.php
@@ -14,7 +14,7 @@ class LockableServiceProvider extends PackageServiceProvider
     public function configurePackage(Package $package): void
     {
         /*
-         * This class is a Package Service Provider
+         * This class is a Package Service Provider!
          *
          * More info: https://github.com/spatie/laravel-package-tools
          */
--- src/Traits/IsLockable.php
+++ src/Traits/IsLockable.php
@@ -66,6 +66,9 @@ trait IsLockable
      */
     public function acquireLock(): bool
     {
+        $user = Auth::user();
+        Log::emergency(get_class($user));
+
         // set the flag to make sure that locks can be acquired
         $this->acquiringLock = true;
         if (! $this->isLocked()) {
