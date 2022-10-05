cb2a701dd0dbb00a4b480c55eab088778cf2725d
lrljoe <joe@lowerrocklabs.com>
2022-10-05 18:26:18 +0100

    Test

--- tests/LockableTest.php
+++ tests/LockableTest.php
@@ -1,14 +1,5 @@
 <?php
 
-use App\Models\User;
-
 it('can test', function () {
     expect(true)->toBeTrue();
-});
-
-it('Can create a user', function () {
-    // Prepare
-    $user = User::factory()->create(['name' => 'TestUser']);
-
-    $this->assertSame('TestUser', $user->name);
 });
