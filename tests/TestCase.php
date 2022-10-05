913265aaf99ea5c2c5f08ab6e191508ff1449823
lrljoe <joe@lowerrocklabs.com>
2022-10-05 18:21:49 +0100

    Tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,9 +16,21 @@ abstract class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->artisan('migrate', ['--database' => 'testbench']);
-        $this->loadMigrationsFrom(__DIR__.'/../migrations');
     }
+
+
+    /**
+ * Define database migrations.
+ *
+ * @return void
+ */
+    protected function defineDatabaseMigrations()
+    {
+        $this->artisan('migrate', ['--database' => 'testbench'])->run();
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->loadLaravelMigrations(['--database' => 'testbench']);
+    }
+
 
     protected function getPackageProviders($app)
     {
@@ -27,26 +39,48 @@ abstract class TestCase extends BaseTestCase
         ];
     }
 
+    /**
+     * Ignore package discovery from.
+     *
+     * @return array<int, array>
+     */
+    public function ignorePackageDiscoveriesFrom()
+    {
+        return [];
+    }
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function defineEnvironment($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+        'driver'   => 'sqlite',
+        'database' => ':memory:',
+        'prefix'   => '',
+    ]);
+
+            // Setup tfhe right User class (using stub)
+            $app['config']->set('auth.providers.users.model', User::class);
+            $app['config']->set('auth.providers.users', [
+                'driver' => 'eloquent',
+                'model' => User::class,
+            ]);
+            $app['config']->set('auth.guards.users', [
+                'driver' => 'session',
+                'provider' => 'admins',
+            ]);
+    }
+
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-        $app['config']->set('database.default', 'testbench');
-        $app['config']->set('database.connections.testbench', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
 
-        // Setup tfhe right User class (using stub)
-        $app['config']->set('auth.providers.users.model', User::class);
-        $app['config']->set('auth.providers.users', [
-            'driver' => 'eloquent',
-            'model' => User::class,
-        ]);
-        $app['config']->set('auth.guards.users', [
-            'driver' => 'session',
-            'provider' => 'admins',
-        ]);
+
 
         /*
         $migration = include __DIR__.'/../database/migrations/create_laravel-lockable_table.php.stub';
