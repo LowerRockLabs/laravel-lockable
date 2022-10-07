e1c96f6207b8e8908fb28545849664ec33c0ac23
lrljoe <joe@lowerrocklabs.com>
2022-10-07 21:54:40 +0100

    Removing Spatie

--- src/LockableServiceProvider.php
+++ src/LockableServiceProvider.php
@@ -5,24 +5,10 @@ namespace LowerRockLabs\Lockable;
 use LowerRockLabs\Lockable\Commands\FlushAll;
 use LowerRockLabs\Lockable\Commands\FlushExpired;
 use LowerRockLabs\Lockable\Models\ModelLock;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class LockableServiceProvider extends PackageServiceProvider
+class LockableServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
-    {
-        /*
-         * This class is a Package Service Provider!!
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('laravel-lockable')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_model_locks_table');
-    }
 
     public function boot()
     {
