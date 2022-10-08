7e6f52bc4caf47d75bc886beb13529c7249713b0
Joe <joe@lowerrocklabs.com>
2022-10-08 03:01:56 +0100

    Adding Controller for Unlocks

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use LowerRockLabs\Lockable\Http\Controllers\LockRequestController;
 
 Route::get('/unlock/{lockRequestID}', [LockRequestController::class, 'unlock'])->name('laravellockable.unlock');
+Route::get('/request/{lockRequestID}', [LockRequestController::class, 'request'])->name('laravellockable.request');
