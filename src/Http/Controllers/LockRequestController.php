925c5eb8b4b588a965565a517f8fd2b5fe50cae5
Joe <joe@lowerrocklabs.com>
2022-10-08 04:04:55 +0100

    Pint Fixes

--- src/Http/Controllers/LockRequestController.php
+++ src/Http/Controllers/LockRequestController.php
@@ -13,38 +13,37 @@ class LockRequestController extends BaseController
     {
         try {
             $modelLock = ModelLock::findOrFail($lockRequestID);
+            if ($modelLock->user_id == Auth::id())
+            {
+                $modelLock->delete();
+            }
         } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
             session()->flash('message', __('error.modelnotfound'));
-
             return false;
         }
-        if ($modelLock->user_id == Auth::id()) {
-            $modelLock->delete();
-        }
+
     }
 
     public function request($lockRequestID)
     {
         try {
             $modelLock = ModelLock::findOrFail($lockRequestID);
+            if (!empty(Auth::user()))
+            {
+                if (!empty($modelLock))
+                {
+                    $mlw = new ModelLockWatcher();
+                    $mlw->model_lock_id = $modelLock->id;
+                    $mlw->user_id = Auth::id();
+                    $mlw->user_type = get_class(Auth::user());
+                    $mlw->save();
+                }
+
+            }
+
         } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
             session()->flash('message', __('error.modelnotfound'));
-
             return false;
-        }
-        if (! empty(Auth::user())) {
-            if (! empty($modelLock)) {
-                $mlw = new ModelLockWatcher();
-                $mlw->model_lock_id = $modelLock->id;
-                $mlw->user_id = Auth::id();
-                $mlw->user_type = get_class(Auth::user());
-                $mlw->save();
-            }
-            //$modelLock->lockWatchers->users->attach(Auth::user());
-            /*$newLock = $modelLock->lockWatchers()->create();
-            $newLock->user_id = Auth::id();
-            $newLock->user_type = get_class(Auth::user());
-            $newLock->save();*/
         }
     }
 }
