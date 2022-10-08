2af5f2f14d40524ba8d0e658221b6c97821fd7fb
lrljoe <joe@lowerrocklabs.com>
2022-10-08 01:05:50 +0100

    Minor amends

--- src/Traits/IsLockable.php
+++ src/Traits/IsLockable.php
@@ -22,7 +22,7 @@ trait IsLockable
         if (config('laravel-lockable.get_locked_on_retrieve', true)) {
             static::retrieved(function (Model $model) {
                 if (! empty($model->lockable)) {
-                    if (!empty($model_>lockable->user))
+                    if (!empty($model->lockable->user))
                     {
                         $model->lockHolderName = $model->lockable->user->name;
                     }
