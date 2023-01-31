aa5da0cbdfc778c3ed7b6ea415406491a535ee01
Joe <joe@lowerrocklabs.com>
2023-01-31 22:13:51 +0000

    Initial Commit

--- config/config.php
+++ config/config.php
@@ -6,23 +6,4 @@ return [
     // Name of the Table containing the locks
     'locks_table' => 'model_locks',
 
-    // Name of the Table containing the lock watchers
-    'lock_watchers_table' => 'model_lock_watchers',
-
-    // Enable retrieval of lock status on retrieving a model
-    'get_locked_on_retrieve' => true,
-
-    // Prevent updating if a model is locked by another user
-    'prevent_updating' => true,
-
-    // Time in Seconds For Lock To Persist
-    'duration' => '3600',
-
-    'scheduled_task_enable' => true,
-
-    // Routes
-    'publish_routes' => true,
-    'prefix' => 'lockable',
-    'middleware' => ['web'],
-
 ];
--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use LowerRockLabs\Lockable\Http\Controllers\LockRequestController;
-
-Route::get('/unlock/{lockRequestID}', [LockRequestController::class, 'unlock'])->name('laravellockable.unlock');
-Route::get('/request/{lockRequestID}', [LockRequestController::class, 'request'])->name('laravellockable.request');
